6f07e50f91d3129a8b6fc99dcd8f402f18c79692
Guokangjie <>
2023-02-24 08:26:24 +0800

    feat(初始化项目): 1、测试release版本

---
+++ src/main/java/cn/gxm/test/controller/TestController.java
@@ -0,0 +1,20 @@
+package cn.gxm.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author GXM
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2023年02月24日
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/say")
+    public String say() {
+        return "hello";
+    }
+}
